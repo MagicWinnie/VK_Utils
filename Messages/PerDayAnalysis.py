6a0f8596c474fdf08d239ca54667af509d191a96
MagicWinnie <magicwinnie@yandex.ru>
2021-02-17 03:13:41 +0700

    Added analysis for messages

--- Messages/PerDayAnalysis.py
+++ Messages/PerDayAnalysis.py
@@ -1,8 +1,203 @@
 '''
 Analyze amount of messages with a person
-Requires downloading a ZIP archive with messages through VKOpt
+* Requires downloading a .zip archive with messages through VKOpt
+as working with messages through VK API became nearly impossible
+* Save the file with following parameters (with all whitespaces):
+`Message format`: 
+%username% (%date%)
+
+`Date format`:
+HH:MM:ss  dd/mm/yyyy
+* Put the .txt files into `data` folder next to the script
 By @MagicWinnie
 '''
 
-PATH = ''
+import os
+from datetime import datetime
+import collections
+import numpy as np
+import matplotlib.pyplot as plt
 
+# 0 - All messages per date
+# 1 - Messages per speaker per date
+# 2 - All messages per time of day
+# 3 - Messages per speaker per time of day
+# 4 - All messages per hour 
+# 5 - Messages per speaker per hour
+TYPE = 5
+assert TYPE in range(6)
+
+def process_string(s: str) -> str:
+    s = s.strip()
+    s = s.rstrip()
+    s = s.lstrip()
+    s = s.replace('\n', '')
+    return s
+
+def return_key(c: collections.Counter) -> str:
+    return c[0]
+
+# TIME = (start, end) => start <= TIME < END
+MORNING = (7, 12)
+DAY     = (12, 18)
+EVENING = (18, 0)
+NIGHT   = (0, 7)
+
+messages = []
+speakers = collections.Counter()
+
+FILES = os.listdir('data')
+for f in FILES:
+    with open(os.path.join('data', f), 'r', encoding='utf-8') as f_b:
+        messages += list(map(process_string, f_b.readlines()))
+        speakers[messages[-1].split()[0] + " " + messages[-1].split()[1]] += 1
+
+speakers = list(map(return_key, speakers.most_common(2)))
+
+if TYPE == 0:   
+    fig, ax = plt.subplots()
+    c = collections.Counter()
+
+    for m in messages:
+        t = m.split()
+        c[datetime.strptime(t[-1][:-1], '%d/%m/%Y')] += 1
+
+    ax.bar(c.keys(), c.values())
+    
+    ax.set_xlabel("Date")
+    ax.set_ylabel("Messages")
+    ax.set_title("All messages per date")
+    plt.show()
+
+if TYPE == 1:
+    fig, ax = plt.subplots(nrows=1, ncols=2, sharey=True)
+
+    c1 = collections.Counter()
+    c2 = collections.Counter()
+    for m in messages:
+        t = m.split()
+        if t[0] + " " + t[1] == speakers[0]:
+            c1[datetime.strptime(t[-1][:-1], '%d/%m/%Y')] += 1
+        else:
+            c2[datetime.strptime(t[-1][:-1], '%d/%m/%Y')] += 1
+
+    ax[0].bar(c1.keys(), c1.values(), color='blue')
+    ax[1].bar(c2.keys(), c2.values(), color='red')
+
+    fig.suptitle("Messages per speaker per date")
+
+    ax[0].set_xlabel("Date")
+    ax[0].set_ylabel("Messages")
+    ax[0].set_title(speakers[0])
+
+    ax[1].set_xlabel("Date")
+    ax[1].set_title(speakers[1])
+    fig.tight_layout()
+    plt.show()
+
+if TYPE == 2:
+    fig, ax = plt.subplots()
+    c = collections.Counter()
+
+    for m in messages:
+        t = m.split()
+        time = datetime.strptime(t[-2][1:], '%H:%M:%S')
+        if time.hour >= NIGHT[0] and time.hour < NIGHT[1]:
+            c['NIGHT'] += 1
+        elif time.hour >= MORNING[0] and time.hour < MORNING[1]:
+            c['MORNING'] += 1
+        elif time.hour >= DAY[0] and time.hour < DAY[1]:
+            c['DAY'] += 1
+        else:
+            c['EVENING'] += 1
+
+    ax.bar(c.keys(), c.values())
+    
+    ax.set_xlabel("Time of day")
+    ax.set_ylabel("Messages")
+    ax.set_title("All messages per time of day")
+    plt.show()
+
+if TYPE == 3:
+    fig, ax = plt.subplots(nrows=1, ncols=2, sharey=True)
+
+    c1 = collections.Counter()
+    c2 = collections.Counter()
+    for m in messages:
+        t = m.split()
+        time = datetime.strptime(t[-2][1:], '%H:%M:%S')
+        if t[0] + " " + t[1] == speakers[0]:
+            if time.hour >= NIGHT[0] and time.hour < NIGHT[1]:
+                c1['NIGHT'] += 1
+            elif time.hour >= MORNING[0] and time.hour < MORNING[1]:
+                c1['MORNING'] += 1
+            elif time.hour >= DAY[0] and time.hour < DAY[1]:
+                c1['DAY'] += 1
+            else:
+                c1['EVENING'] += 1
+        else:
+            if time.hour >= NIGHT[0] and time.hour < NIGHT[1]:
+                c2['NIGHT'] += 1
+            elif time.hour >= MORNING[0] and time.hour < MORNING[1]:
+                c2['MORNING'] += 1
+            elif time.hour >= DAY[0] and time.hour < DAY[1]:
+                c2['DAY'] += 1
+            else:
+                c2['EVENING'] += 1
+
+    ax[0].bar(c1.keys(), c1.values(), color='blue')
+    ax[1].bar(c2.keys(), c2.values(), color='red')
+
+    fig.suptitle("Messages per speaker per time of day")
+
+    ax[0].set_xlabel("Time of day")
+    ax[0].set_ylabel("Messages")
+    ax[0].set_title(speakers[0])
+
+    ax[1].set_xlabel("Time of day")
+    ax[1].set_title(speakers[1])
+    fig.tight_layout()
+    plt.show()
+
+if TYPE == 4:
+    fig, ax = plt.subplots()
+    c = collections.Counter()
+
+    for m in messages:
+        t = m.split()
+        time = datetime.strptime(t[-2][1:], '%H:%M:%S')
+        c[time.hour] += 1
+        
+    ax.bar(c.keys(), c.values())
+    
+    ax.set_xlabel("Hour")
+    ax.set_ylabel("Messages")
+    ax.set_title("All messages per hour ")
+    plt.show()
+
+if TYPE == 5:
+    fig, ax = plt.subplots(nrows=1, ncols=2, sharey=True)
+
+    c1 = collections.Counter()
+    c2 = collections.Counter()
+    for m in messages:
+        t = m.split()
+        time = datetime.strptime(t[-2][1:], '%H:%M:%S')
+        if t[0] + " " + t[1] == speakers[0]:
+            c1[time.hour] += 1
+        else:
+            c2[time.hour] += 2
+
+    ax[0].bar(c1.keys(), c1.values(), color='blue')
+    ax[1].bar(c2.keys(), c2.values(), color='red')
+
+    fig.suptitle("Messages per speaker per hour")
+
+    ax[0].set_xlabel("Hour")
+    ax[0].set_ylabel("Messages")
+    ax[0].set_title(speakers[0])
+
+    ax[1].set_xlabel("Hour")
+    ax[1].set_title(speakers[1])
+    fig.tight_layout()
+    plt.show()
