8be4589de1117f3dbb81a21f11a8bcbde031cea0
MagicWinnie <magicwinnie@yandex.ru>
2022-11-06 22:59:20 +0300

    Modified for autostatus

--- AutoStatus/AutoStatus.py
+++ AutoStatus/AutoStatus.py
@@ -8,34 +8,26 @@ import time
 import datetime
 import json
 import vk_api
-import requests
-from argparse import ArgumentParser
-
-parser = ArgumentParser()
-parser.add_argument("text", type=str, help="Event text")
-parser.add_argument("date", type=datetime.date.fromisoformat, help="Event date")
-parser.add_argument(
-    "--login",
-    type=str,
-    default="../LoginData/login.json",
-    help="path to login.json (default: ../LoginData/login.json)",
-)
-parser.add_argument(
-    "--delay",
-    type=int,
-    default=10,
-    help="delay between status updates (default: 10)",
-)
-args = parser.parse_args()
+from argparse import ArgumentParser, ArgumentTypeError
 
 
 def auth_handler(remember_device: bool = True):
     key = input("Enter authentication code: ")
     return key, remember_device
 
+
 def captcha_handler(captcha):
     key = input("Enter captcha code {0}: ".format(captcha.get_url())).strip()
     return captcha.try_again(key)
+
+
+def valid_date(s):
+    try:
+        return datetime.datetime.strptime(s, "%d.%m.%Y %H:%M")
+    except ValueError:
+        msg = "not a valid date: {0!r}".format(s)
+        raise ArgumentTypeError(msg)
+
 
 def right_ending(m, t):
     endings = {
@@ -66,7 +58,6 @@ def secs2dhm(seconds):
 def runner(NOW, END_DATE):
     global args
     TEXT = "До {} осталось: {} {} {} {}"
-
     d, h, m, s = secs2dhm(dateDiffInSeconds(NOW, END_DATE))
     text = TEXT.format(
         args.text,
@@ -75,11 +66,36 @@ def runner(NOW, END_DATE):
         right_ending(m, "m"),
         right_ending(s, "s"),
     )
-
     return text
 
 
-assert os.path.isfile(args.login), "[ERROR] {} is not a file or it does not exist!".format(args.login)
+parser = ArgumentParser()
+parser.add_argument(
+    "text",
+    type=str,
+    help="Event text"
+)
+parser.add_argument(
+    "date",
+    type=valid_date,
+    help="Event date in format: DD.MM.YYYY HH:MM"
+)
+parser.add_argument(
+    "--login",
+    type=str,
+    default="../LoginData/login.json",
+    help="path to login.json (default: ../LoginData/login.json)",
+)
+parser.add_argument(
+    "--delay",
+    type=int,
+    default=10,
+    help="delay between status updates (default: 10)",
+)
+args = parser.parse_args()
+
+assert os.path.isfile(
+    args.login), "[ERROR] {} is not a file or it does not exist!".format(args.login)
 with open(args.login, "r") as f:
     j = json.load(f)
 
@@ -88,27 +104,19 @@ assert "pass" in j, "[ERROR] `pass` key does not exist!"
 login = j["login"]
 password = j["pass"]
 
-vk_session = vk_api.VkApi(login=login, password=password, auth_handler=auth_handler, captcha_handler=captcha_handler)
-try:
-    vk_session.auth()
-except requests.ConnectionError:
-    print("[ERROR] No internet connection")
-    exit(-1)
-except Exception as e:
-    print("[ERROR] " + str(e))
-    exit(-1)
+vk_session = vk_api.VkApi(
+    login=login, password=password,
+    auth_handler=auth_handler, captcha_handler=captcha_handler
+)
+vk_session.auth(token_only=True)
 vk = vk_session.get_api()
 
 END_DATE = args.date
-try:
-    END_DATE.hour
-except AttributeError:
-    END_DATE = datetime.datetime.combine(END_DATE, datetime.time(0, 0))
 DELAY = args.delay
 NOW = datetime.datetime.now()
-PREV = time.time()
+PREV = -1e10
 TRIES = 0
-print("[INFO] INIT FINISHED")
+print("[INFO] Timer started.")
 while END_DATE > NOW:
     if TRIES > 5:
         break
@@ -118,8 +126,8 @@ while END_DATE > NOW:
             vk.status.set(text=text)
             TRIES = 0
         except ConnectionError:
-            print("[ERROR] No internet connection")
+            print("[ERROR] No internet connection.")
             TRIES += 1
         NOW = datetime.datetime.now()
         PREV = time.time()
-print("[INFO] TIMER FINISHED")
+print("[INFO] Timer finished.")
